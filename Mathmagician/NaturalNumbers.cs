330341461326da8b58db1dbb14515ccce531b9de
danielcossins11 <daniel.cossins@yahoo.com>
2015-10-23 14:30:45 -0500

    added NaturalNumbers

---
+++ Mathmagician/NaturalNumbers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class NaturalNumbers
+    {
+        private int first;
+
+        public NaturalNumbers()
+        {
+            first = 1;
+        }
+
+        public int GetNext(int x)
+        {
+            return x + 1;
+        }
+
+        public int GetFirst()
+        {
+            return first;
+        }
+
+        public int[] GetSequence()
+        {
+            return GetSequence(10);
+        }
+
+        public int[] GetSequence(int length)
+        {
+            //int[] output = new int[length];
+            List<int> output = new List<int>();
+            output.Add(GetFirst());
+            while (output.Count < length)
+            {
+                output.Add(GetNext(output.Last()));
+            }
+            return output.ToArray();
+        }
+
+        public string ToString(int[] array)
+        {
+            string output = String.Join(" ", array);
+            //string output = "";
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    output += array[i].ToString() + " ";
+            //}
+            return output.TrimEnd();
+        }
+    }
+}
