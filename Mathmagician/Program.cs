c65ec216b3ed235b4917df1baf924449e3ef70a7
danielcossins11 <daniel.cossins@yahoo.com>
2015-10-22 19:02:44 -0500

    beginning program logic

--- Mathmagician/Program.cs
+++ Mathmagician/Program.cs
@@ -10,6 +10,53 @@ namespace Mathmagician
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What would you like for me to do?");
+            int inputInt;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out inputInt))
+                {
+                    if(inputInt == 1 || inputInt == 2 || inputInt == 3)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            }
+
+
+
+
+
+
+            switch (inputInt)
+            {
+                case 1:
+                    Console.WriteLine("Case 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Case 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Case 3");
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+
+
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
