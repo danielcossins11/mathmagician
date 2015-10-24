9d16718770646f2e51acc3468a26a5ee8560d074
danielcossins11 <daniel.cossins@yahoo.com>
2015-10-24 12:39:32 -0500

    made primes class. tests not running

---
+++ Mathmagician/PrimeNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class PrimeNumbers : NaturalNumbers
+    {
+        public PrimeNumbers()
+        {
+            first = 2;
+        }
+
+        public override int GetNext(int x)
+        {
+            int nextPrime =0;
+            for(int i=x+2; i<1000; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    for(int j=2; j<=i/2 + 1; i++)
+                    {
+                        if (i%j == 0)
+                        {
+                            nextPrime = i;
+                        }
+                    }
+                }
+            }
+            //for(int i=x; i<1000; i++)
+            //{
+            //    if(i.isPrime)
+            //}
+            return nextPrime;
+        }
+    }
+}
---
+++ MathmagicianTests/PrimeTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mathmagician;
+
+namespace MathmagicianTests
+{
+    [TestClass]
+    public class PrimeTests
+    {
+        [TestMethod]
+        public void PrimeEnsureICanCreateClassInstance()
+        {
+            PrimeNumbers prime = new PrimeNumbers();
+            Assert.IsNotNull(prime);
+        }
+
+        [TestMethod]
+        public void PrimeShouldPrintFirstNumber()
+        {
+            PrimeNumbers primes = new PrimeNumbers();
+            int expected = 2;
+            int actual = primes.GetFirst();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PrimesShouldPrintNextNumber()
+        {
+            PrimeNumbers primes = new PrimeNumbers();
+            int input = 2;
+            int expected = 3;
+            int actual = primes.GetNext(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PrimesShouldPrintAVariableSequenceOfNumbers()
+        {
+            PrimeNumbers primes = new PrimeNumbers();
+            int input = 8;
+            int[] expected = new int[] { 2, 3, 5, 7, 11, 13};
+            int[] actual = primes.GetSequence(input);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PrimesPrintSequenceOfNumbers()
+        {
+            PrimeNumbers primes = new PrimeNumbers();
+            int length = 7;
+            string expected = "2 3 5 7 11";
+            string actual = primes.ToString(primes.GetSequence(length));
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
