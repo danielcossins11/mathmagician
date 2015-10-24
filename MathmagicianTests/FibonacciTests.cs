61119ce41600dac72f098db81d2f30a22a8dd803
danielcossins11 <daniel.cossins@yahoo.com>
2015-10-24 11:44:12 -0500

    added fibonacci and tests pass

---
+++ MathmagicianTests/FibonacciTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mathmagician;
+
+namespace MathmagicianTests
+{
+    [TestClass]
+    public class FibonacciTests
+    {
+        [TestMethod]
+        public void FibEnsureICanCreateClassInstance()
+        {
+            FibonacciNumbers fib = new FibonacciNumbers();
+            Assert.IsNotNull(fib);
+        }
+
+        [TestMethod]
+        public void FibShouldPrintFirstNumber()
+        {
+            FibonacciNumbers fib = new FibonacciNumbers();
+            int expected = 1;
+            int actual = fib.GetFirst();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FibShouldPrintNextNumber()
+        {
+            FibonacciNumbers fib = new FibonacciNumbers();
+            int input = 1;
+            int expected = 1;
+            int actual = fib.GetNext(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FibShouldPrintAVariableSequenceOfNumbers()
+        {
+            FibonacciNumbers fib = new FibonacciNumbers();
+            int input = 8;
+            int[] expected = new int[] { 1, 1, 2, 3, 5, 8, 13, 21 };
+            int[] actual = fib.GetSequence(input);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FibPrintSequenceOfNumbers()
+        {
+            FibonacciNumbers fib = new FibonacciNumbers();
+            int length = 7;
+            string expected = "1 1 2 3 5 8 13";
+            string actual = fib.ToString(fib.GetSequence(length));
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
