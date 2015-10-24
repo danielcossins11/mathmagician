a98bda6887f365453796a96ad9f867cf75053b39
danielcossins11 <daniel.cossins@yahoo.com>
2015-10-24 11:14:49 -0500

    built OddNumbers class with tests

---
+++ Mathmagician/OddNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class OddNumbers : NaturalNumbers
+    {
+        public OddNumbers()
+        {
+            first = 1;
+            increment = 2;
+        }
+
+        public override int[] GetSequence(int length)
+        {
+            //int[] output = new int[length];
+            List<int> output = new List<int>();
+            output.Add(GetFirst());
+            while (output.Count < length)
+            {
+                output.Add(GetNext(output.Last()));
+            }
+            return output.ToArray();
+        }
+    }
+}
---
+++ MathmagicianTests/OddTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mathmagician;
+
+namespace MathmagicianTests
+{
+    [TestClass]
+    public class OddTests
+    {
+        [TestMethod]
+        public void OddEnsureICanCreateClassInstance()
+        {
+            OddNumbers odd = new OddNumbers();
+            Assert.IsNotNull(odd);
+        }
+
+        [TestMethod]
+        public void OddShouldPrintFirstNumber()
+        {
+            OddNumbers odds = new OddNumbers();
+            int expected = 1;
+            int actual = odds.GetFirst();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OddShouldPrintNextNumber()
+        {
+            OddNumbers odds = new OddNumbers();
+            int input = 1;
+            int expected = 3;
+            int actual = odds.GetNext(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OddShouldPrintAVariableSequenceOfNumbers()
+        {
+            OddNumbers odds = new OddNumbers();
+            int input = 8;
+            int[] expected = new int[] { 1, 3, 5, 7, 9, 11, 13, 15 };
+            int[] actual = odds.GetSequence(input);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OddPrintSequenceOfNumbers()
+        {
+            OddNumbers odds = new OddNumbers();
+            int length = 7;
+            string expected = "1 3 5 7 9 11 13";
+            string actual = odds.ToString(odds.GetSequence(length));
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
