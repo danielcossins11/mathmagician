3eec10d42c25f496cce1ddfb751bf6d8f674fdca
danielcossins11 <daniel.cossins@yahoo.com>
2015-10-26 11:55:04 -0500

    refactored program.cs

--- Mathmagician/Program.cs
+++ Mathmagician/Program.cs
@@ -9,6 +9,42 @@ namespace Mathmagician
 {
     class Program
     {
+        public static string getValidstring()
+        {
+            string input;
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input.ToLower() == "natural numbers" || input.ToLower() == "even numbers" || input.ToLower() == "odd numbers" || input.ToLower() == "fibonacci numbers" || input.ToLower() == "prime numbers")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            }
+            return input;
+        }
+
+        public static int getValidInt()
+        {
+            int inputInt;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out inputInt))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            }
+            return inputInt;
+        }
+
         static void Main(string[] args)
         {
             string prompt = "> ";
@@ -19,27 +55,55 @@ Odd Numbers
 Fibonacci Numbers
 Prime Numbers");
             Console.Write(prompt);
-            string response = Console.ReadLine();
-            if (response.ToLower() == "natural numbers")
+            string input = getValidstring();
+            Console.WriteLine("How many?");
+            int inputInt = getValidInt();
+            //if (response.ToLower() == "natural numbers")
+            //{
+            //    Console.WriteLine("How many?");
+            //    Console.Write(prompt);
+            //    string response_length = Console.ReadLine();
+            //    int length;
+            //    bool parsed = int.TryParse(response_length, out length);
+            //    if (parsed)
+            //    {
+            //        NaturalNumbers nats = new NaturalNumbers();
+            //        Console.WriteLine(nats.ToString(nats.GetSequence(length)));
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Whoops!");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Nope! Do better next time.");
+            //}
+            switch (input.ToLower())
             {
-                Console.WriteLine("How many?");
-                Console.Write(prompt);
-                string response_length = Console.ReadLine();
-                int length;
-                bool parsed = int.TryParse(response_length, out length);
-                if (parsed)
-                {
+                case "natural numbers":
                     NaturalNumbers nats = new NaturalNumbers();
-                    Console.WriteLine(nats.ToString(nats.GetSequence(length)));
-                }
-                else
-                {
+                    Console.WriteLine(nats.ToString(nats.GetSequence(inputInt)));
+                    break;
+                case "even numbers":
+                    EvenNumbers evens = new EvenNumbers();
+                    Console.WriteLine(evens.ToString(evens.GetSequence(inputInt)));
+                    break;
+                case "odd numbers":
+                    OddNumbers odds = new OddNumbers();
+                    Console.WriteLine(odds.ToString(odds.GetSequence(inputInt)));
+                    break;
+                case "fibonacci numbers":
+                    FibonacciNumbers fibs = new FibonacciNumbers();
+                    Console.WriteLine(fibs.ToString(fibs.GetSequence(inputInt)));
+                    break;
+                case "prime numbers":
+                    PrimeNumbers primes = new PrimeNumbers();
+                    Console.WriteLine(primes.ToString(primes.GetSequence(inputInt)));
+                    break;
+                default:
                     Console.WriteLine("Whoops!");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Nope! Do better next time.");
+                    break;
             }
             Console.WriteLine("Press any key to finish . . .");
             Console.ReadKey();
